d4aa28b55775ba08668c81a2a3b3d6660310a36c
Juan J. San Emeterio <emeterio@Juans-MacBook-Pro.local>
2017-03-29 21:46:48 -0400

    added headers and lowered image quality further

--- src/pages/general-information/general-information.ts
+++ src/pages/general-information/general-information.ts
@@ -76,7 +76,7 @@ export class GeneralInformationPage {
 
   captureImage(){
     let options = {
-                    "quality": 50,
+                    "quality": 35,
                     "destinationType": Camera.DestinationType.DATA_URL,
                     "encodingType": Camera.EncodingType.JPEG
                   }
