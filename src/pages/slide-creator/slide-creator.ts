fd749207e7372b103c402cc793b21ecc3b5989b8
Juan J San Emeterio <juanjser@gmail.com>
2017-04-19 17:35:52 -0400

    Lowered quality.

--- src/pages/slide-creator/slide-creator.ts
+++ src/pages/slide-creator/slide-creator.ts
@@ -38,9 +38,8 @@ export class SlideCreatorPage {
 
   captureImage() {
     let options = {
-      "quality": 25,
-      "destinationType": this.camera.DestinationType.DATA_URL,
-      "encodingType": this.camera.EncodingType.JPEG
+      "quality": 1,
+      "destinationType" :0
     }
     this.camera.getPicture(options).then((imageData) => {
       this.base64Img["img"] = 'data:image/jpeg;base64,' + imageData;
