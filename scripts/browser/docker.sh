272b0c08ea2ec0f9bb2a84a3c4e5b87db6c341c2
Juan J San Emeterio <juanjser@gmail.com>
2017-05-01 05:33:11 -0400

    improved docker build script

--- scripts/browser/docker.sh
+++ scripts/browser/docker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+ionic build browser --prod
 rm -rf ../invisibox-service/docker/nginx/www 
+rm platforms/browser/www/index.html
+cp scripts/browser/index.html platforms/browser/www/index.html
 cp -R platforms/browser/www ../invisibox-service/docker/nginx/www
 docker rmi -f invisibox-ui 
 docker build -t invisibox-ui ../invisibox-service/docker/nginx
